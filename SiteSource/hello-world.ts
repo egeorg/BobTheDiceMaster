fa22d120038db876b15ea1d0186c7dd5422a9063
Georgii Erokhin <GErokhin@foss.dk>
2022-07-24 10:29:51 +0300

    testing using wasm from html/js

---
+++ SiteSource/hello-world.ts
@@ -0,0 +1,8 @@
+// An example from https://wasmbyexample.dev/examples/hello-world/hello-world.assemblyscript.en-us.html
+
+// This exports an add function.
+// It takes in two 32-bit integer values
+// And returns a 32-bit integer value.
+export function add(a: i32, b: i32): i32 {
+  return a + b;
+}
