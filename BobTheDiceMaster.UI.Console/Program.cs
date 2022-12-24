551c5b0751480a5fc818ec8587a5d3923f00f0bf
egeorg <george.erokhin@gmail.com>
2022-12-25 03:22:24 +0500

    Use System.Console instead of Console to get rid of collisions with namespace

--- BobTheDiceMaster.UI.Console/Program.cs
+++ BobTheDiceMaster.UI.Console/Program.cs
@@ -14,25 +14,26 @@ namespace BobTheDiceMaster.UI.Console
       //  Console.WriteLine($"{{{combination}, {DiceRoll.AverageScore(combination):R}}}");
       //}
       TestVerboseBob();
-      Console.ReadLine();
-      Console.WriteLine("Starting a game...");
+      System.Console.ReadLine();
+      System.Console.WriteLine("Starting a game...");
       //GameOfSchool game = new GameOfSchool(new HumanPlayer());
       GameOfSchool game = new GameOfSchool(new VerboseBruteForceBob());
 
       while (!game.IsGameOver)
       {
-        Console.WriteLine();
-        Console.WriteLine("Performing next step");
+        System.Console.WriteLine();
+        System.Console.WriteLine("Performing next step");
         PerformNextStep(game);
         if (!game.IsGameOver)
         {
+          System.
           Console.WriteLine($"Scored. Current score: {game.Score}");
         }
         else
         {
-          Console.WriteLine($"Game over! Score is {game.Score}");
+          System.Console.WriteLine($"Game over! Score is {game.Score}");
         }
-        Console.ReadKey();
+        System.Console.ReadKey();
       }
     }
 
@@ -41,11 +42,11 @@ namespace BobTheDiceMaster.UI.Console
       DiceRoll roll = game.GenerateRoll().Roll;
       while (!game.IsTurnOver)
       {
-        Console.WriteLine($"Considering roll {roll}. Waiting for a decision.");
+        System.Console.WriteLine($"Considering roll {roll}. Waiting for a decision.");
 
         Decision decision = game.GenerateAndApplyDecision();
 
-        Console.WriteLine(
+        System.Console.WriteLine(
           Environment.NewLine +
           $"Decision is {decision}" + Environment.NewLine +
           $"Best combinations are:" + Environment.NewLine +
@@ -66,7 +67,7 @@ namespace BobTheDiceMaster.UI.Console
         {
           pSum += reroll.GetProbability();
         }
-        Console.WriteLine($"{i} dice rerolls probability sum: {pSum}");
+        System.Console.WriteLine($"{i} dice rerolls probability sum: {pSum}");
       }
     }
 
@@ -75,8 +76,8 @@ namespace BobTheDiceMaster.UI.Console
       DiceRoll r1 = new DiceRoll(new[] { 1, 1, 6, 6, 6 });
       DiceRoll r2 = new DiceRoll(new[] { 6, 1, 6, 1, 6 });
       DiceRoll r3 = new DiceRoll(new[] { 1, 2, 3, 4, 5 });
-      Console.WriteLine(r1.Equals(r2));
-      Console.WriteLine(r1.Equals(r3));
+      System.Console.WriteLine(r1.Equals(r2));
+      System.Console.WriteLine(r1.Equals(r3));
     }
 
     static void TestVerboseBob()
@@ -98,11 +99,11 @@ namespace BobTheDiceMaster.UI.Console
         currentRoll: new DiceRoll(new[] { 2, 2, 4, 4, 6 }),
         rerollsLeft: 3);
 
-      Console.WriteLine(
+      System.Console.WriteLine(
         $"Best combinations are: {Environment.NewLine}" +
         $"{string.Join(Environment.NewLine, decision.RatedDecisionInfo)}");
 
-      Console.WriteLine(decision);
+      System.Console.WriteLine(decision);
     }
   }
 }
