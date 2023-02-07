f2f9d7c46e7b313ceb999ba5983cfc5755bce6cc
egeorg <george.erokhin@gmail.com>
2023-02-08 01:06:41 +0500

    Add combinatorics unit tests and add verification logic to combinatorics code

---
+++ BobTheDiceMaster.Test/TestCombinatorics.cs
@@ -0,0 +1,94 @@
+﻿namespace BobTheDiceMaster.Test
+{
+  public class TestCombinatorics
+  {
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(720, 6)]
+    [InlineData(3628800, 10)]
+    public void Factorial_IsCorrect_ForPositiveValues(long expectedFactorial, int factorialArgument)
+    {
+      Assert.Equal(expectedFactorial, Combinatorics.Factorial(factorialArgument));
+    }
+
+    [Fact]
+    public void Factorial_Is1_For0()
+    {
+      Assert.Equal(1, Combinatorics.Factorial(0));
+    }
+
+    [Fact]
+    public void Factorial_DoesNotOverflow_ForMaximalAllowedArgument()
+    {
+      Assert.Equal(2432902008176640000, Combinatorics.Factorial(20));
+    }
+
+    [Fact]
+    public void Factorial_ThrowsArgumentException_ForNegativeArgument()
+    {
+      Assert.Throws<ArgumentException>(() => Combinatorics.Factorial(-1));
+    }
+
+    [Fact]
+    public void Factorial_ThrowsArgumentException_ForMinimalForbiddenArgument()
+    {
+      Assert.Throws<ArgumentException>(() => Combinatorics.Factorial(21));
+    }
+
+    [Theory]
+    [InlineData(3, 3, 2)]
+    [InlineData(9, 9, 1)]
+    [InlineData(252, 10, 5)]
+    public void NumberOfCombinations_IsCorrect_ForPositiveValues(int expectedResult, int n, int k)
+    {
+      Assert.Equal(expectedResult, Combinatorics.Cnk(n, k));
+    }
+
+    /// <summary>
+    /// The results are not so big to be worried about, but big numbers may appear during
+    /// internal calculations if the algorithm is not smart enough.
+    /// </summary>
+    [Theory]
+    [InlineData(1, 20, 0)]
+    [InlineData(1, 20, 20)]
+    [InlineData(184756, 20, 10)]
+    public void NumberOfCombinaitons_DoesNotOverflow_ForMaximalAllowedArguments(int expectedResult, int n, int k)
+    {
+      Assert.Equal(expectedResult, Combinatorics.Cnk(n, k));
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(11)]
+    public void NumberOfCombinations_Is1_ForKEqual0(int n)
+    {
+      Assert.Equal(1, Combinatorics.Cnk(n, 0));
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(12)]
+    public void NumberOfCombinations_Is1_ForKEqualN(int n)
+    {
+      Assert.Equal(1, Combinatorics.Cnk(n, n));
+    }
+
+    [Fact]
+    public void NumberOfCombinaitons_ThrowsArgumentException_ForMinimalForbiddenN()
+    {
+      Assert.Throws<ArgumentException>(() => Combinatorics.Cnk(21, 0));
+    }
+
+    [Fact]
+    public void NumberOfCombinaitons_ThrowsArgumentException_ForNegativeN()
+    {
+      Assert.Throws<ArgumentException>(() => Combinatorics.Cnk(-1, 0));
+    }
+
+    [Fact]
+    public void NumberOfCombinaitons_ThrowsArgumentException_ForNegativeK()
+    {
+      Assert.Throws<ArgumentException>(() => Combinatorics.Cnk(0, -1));
+    }
+  }
+}
--- BobTheDiceMaster/Combinatorics.cs
+++ BobTheDiceMaster/Combinatorics.cs
@@ -1,9 +1,39 @@
-﻿namespace BobTheDiceMaster
+﻿using System;
+
+namespace BobTheDiceMaster
 {
   public class Combinatorics
   {
+    private const int factorialMaxArgument = 20;
+
+    /// <summary>
+    /// This method is rather dumb and it's not suitable for big numbers.
+    /// </summary>
     public static long Cnk(int n, int k)
     {
+      if (n > factorialMaxArgument)
+      {
+        // There are some situations when it's fine to have big n (e.g. if n == k), but
+        // they should not appear in a program like this, it's fine to have a restriction like this.
+        throw new ArgumentException(
+          $"{nameof(n)} has to be less or equal to {factorialMaxArgument}, otherwise internal {nameof(Cnk)} internal computation may result in an overflow",
+          nameof(n));
+      }
+
+      if (n < 0)
+      {
+        throw new ArgumentException(
+          $"{nameof(n)} has to be positive, otherwise {nameof(Cnk)} does not makes sense.",
+          nameof(n));
+      }
+
+      if (k < 0 || k > n)
+      {
+        throw new ArgumentException(
+          $"{nameof(k)} has to be between 0 and {nameof(n)}, otherwise {nameof(Cnk)} does not makes sense.",
+          nameof(k));
+      }
+
       long result = 1;
       // Calculate n!/k!
       for (int i = n; i > k; --i)
@@ -16,6 +46,21 @@
 
     public static long Factorial(int n)
     {
+
+      if (n < 0)
+      {
+        throw new ArgumentException(
+          $"{nameof(n)} has to be positive, otherwise {nameof(Factorial)} does not makes sense.",
+          nameof(n));
+      }
+
+      if (n > factorialMaxArgument)
+      {
+        throw new ArgumentException(
+          $"{nameof(n)} was {n}, but it has to be less or equal to {factorialMaxArgument}, otherwise its result would overflow a \"long\" .NET type",
+          nameof(n));
+      }
+
       long result = 1;
       for (int i = n; i > 0; --i)
       {
